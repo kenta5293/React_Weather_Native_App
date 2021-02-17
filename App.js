fa6f3951ecde38fcce6dcc601c4ff205aa232fbb
kenta5293 <kenta.03.1110@gmail.com>
2021-02-17 22:39:33 +0900

    Chapter #1.4 Clear!

--- App.js
+++ App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { Alert } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import axios from 'axios';
 
 // export default function App() {
 //   return <Loading />;
 // }
 
+const API_KEY = '57a2e00d0c1d6bb53c75d40646955aec';
+
 export default class extends React.Component {
   state = {
     isLoading: true,
+  };
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+    console.log(data);
   };
 
   getLocation = async () => {
@@ -18,6 +26,7 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert('오류가 발생했어요', '위치권한이 필요해요.');
