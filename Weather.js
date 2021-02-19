83356b371ea60b0bf044ab66b2b3506bc7313c8b
kenta5293 <kenta.03.1110@gmail.com>
2021-02-20 01:08:18 +0900

    Chapter #2.2 Clear!

--- Weather.js
+++ Weather.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Weather({ temp, condition }) {
   return (
     <View style={styles.container}>
-      <Text>{temp}</Text>
-      <Text>{condition}</Text>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons name="weather-lightning-rainy" size={96} />
+        <Text style={styles.temp}>{temp}°</Text>
+      </View>
+      <View style={styles.halfContainer}></View>
     </View>
   );
 }
@@ -18,6 +22,14 @@ Weather.propTypes = {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  temp: {
+    fontSize: 28,
+  },
+  halfContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
