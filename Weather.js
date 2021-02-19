c4edc995d833a25daa6578dc6f64bbdd663bca28
kenta5293 <kenta.03.1110@gmail.com>
2021-02-19 23:22:33 +0900

    Chapter #2.1 Clear!

--- Weather.js
+++ Weather.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 
-export default function Weather({ temp }) {
+export default function Weather({ temp, condition }) {
   return (
     <View style={styles.container}>
       <Text>{temp}</Text>
+      <Text>{condition}</Text>
     </View>
   );
 }
 
 Weather.propTypes = {
   temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf(['Thunderstorm', 'Drizzle', 'Rain', 'Snow', 'Atmosphere', 'Clear', 'Clouds', 'Haze', 'Mist', 'Dust']).isRequired,
 };
 
 const styles = StyleSheet.create({
